d32c6ec51cede8f06d55da55882d76e0b57cd178
Irma Ivanova <nowa.mir@yandex.ru>
2019-11-14 13:31:46 +0300

    Updates

---
+++ precache-manifest.9782b62be9357397a6327ac97b991ad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0fc51765fd97a52331955adccad466",
+    "url": "/something.github.io/index.html"
+  },
+  {
+    "revision": "a74cc1c7e1221f0664bc",
+    "url": "/something.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7f3fd78fb35597024afc",
+    "url": "/something.github.io/static/js/2.0a84894c.chunk.js"
+  },
+  {
+    "revision": "a74cc1c7e1221f0664bc",
+    "url": "/something.github.io/static/js/main.7b3245b3.chunk.js"
+  },
+  {
+    "revision": "c9c6991a2f780e9f0c2c",
+    "url": "/something.github.io/static/js/runtime-main.611aa07f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/something.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
